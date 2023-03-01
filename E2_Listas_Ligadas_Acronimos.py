e726b8c6c6ba38ed3fe534f4bb66b24b9214ce44
RCJK33 <66058913+RCJK33@users.noreply.github.com>
2023-03-01 13:26:56 -0800

    V2.1
    * El codigo ahora es mas eficiente, recurre a menos validaciones u operaciones boleanas.
    * Ahora, el codigo es 48% mas reducido.
    * El codigo es mas limpio.
    
    - Se elimininaron 5 lineas de codigo de la validacion de acronimos.
    - Se eliminarion funciones para imprimir, ordenar y retornar un nodo en especifico.
    - El programa ahora ofrece menos opciones, ahora solo hace su tarea.

--- E2_Listas_Ligadas_Acronimos.py
+++ E2_Listas_Ligadas_Acronimos.py
@@ -8,58 +8,28 @@ class LinkedList:
         self.head = None
         self.tail = None
         self.length = 0
-
-    def appendL(self,chars):
-        inicio = 0
-        chars = str(chars).upper().replace(" ","")
-        for i in range(inicio,len(chars)):
-            self._auxAppendL(chars[i])
     
-    def simpleAppendL(self,chars):
+    def sortedAppendL(self,chars):
         inicio = 0
-        chars = sorted(str(chars).upper().replace(" ",""))
+        chars = sorted(str(chars).replace(" ","").upper())
         for i in range(inicio,len(chars)):
             self._auxAppendL(chars[i])
-
-    def printL(self):
-        if self.head is not None:
-            nodosIn = self.head
-            while nodosIn.next:
-                print(nodosIn.value)
-                nodosIn = nodosIn.next
-            print(nodosIn.value)  
-        else:
-            print("No hay nodos:printL")
         
     def isAcronymL(self,otherself):
-        if isinstance(otherself,LinkedList):
-            nodosIn = self.head
-            nodosInOther = otherself.head
-            if self.length == otherself.length:
-                """ si se requiere hacer el sort aqui """
-                for i in range(self.length):
-                    evaluador = nodosIn.value == nodosInOther.value
-                    if evaluador:
-                        nodosIn = nodosIn.next
-                        nodosInOther = nodosInOther.next
-                    else:
-                        return False
-                return True
-        return False
-
-    """ def sortL(self):
-        nodoIn = self.head
-        pivote = nodoIn.value
-        i=0
-        ifor = 1
-        while nodoIn.next:
-            if pivote > nodoIn.next.value:
-                self._flip(i,ifor) """
-
-    def _flip(self,position_1,position_2):
-        tempA = self._auxPositioner(position_1)
-        tempB = self._auxPositioner(position_2)
-        tempA.value, tempB.value = tempB.value, tempA.value
+        if not isinstance(otherself,LinkedList):
+            return False
+        if self.length != otherself.length:
+            return False
+        
+        nodosIn = self.head
+        nodosInOther = otherself.head
+        for i in range(self.length):
+            if nodosIn.value == nodosInOther.value:
+                nodosIn = nodosIn.next
+                nodosInOther = nodosInOther.next
+            else:
+                return False
+        return True
 
     def _auxAppendL(self, value):
         if self.head is None:
@@ -70,31 +40,18 @@ class LinkedList:
             self.tail = self.tail.next
         self.length+=1
 
-    def _auxPositioner(self,position):
-        if self.head is not None and position < self.length:
-            nodosIn = self.head
-            for i in range(position):
-                nodosIn = nodosIn.next
-            return nodosIn
-
-
 """ 
-appendL(*value)         => Añade el valor del parameto o parametros a la lista.
 simpleAppendL(*value)   => Añade el valor del parameto o parametros a la lista de forma ordenada con funcion sorted() (funcion ya hecha).
-printL()                => Imprime el valor de todos los elementos en la lista.
-isAcronymL(LinkedList)   => Recibe una instancia de la misma clase y retorna un valor voleano si ambas listas son iguales.
-[Metodo sort() en proceso]
-_flip(position,position)=> Metodo privado que intercambia los valores de los nodos de las posiciones especificas
+isAcronymL(LinkedList)  => Recibe una instancia de la misma clase y retorna un valor voleano si ambas listas son iguales.
 _auxAppendL()           => Metodo privado que añade valores a la lista.
-_auxPositioner()        => Metodo privado que retorna un Nodo en especifico.
 """
 
 my_linked_List = [LinkedList(),LinkedList()]
 
 for i in range(len(my_linked_List)):
-    my_linked_List[i].simpleAppendL(str(input(f"Ingresa la palabra {i+1}: ")))
+    my_linked_List[i].sortedAppendL(input(f"Ingresa la palabra {i+1}: "))
 
 if my_linked_List[0].isAcronymL(my_linked_List[1]):
     print("Son acronimos")
 else:
-    print("No son acronimos")
+    print("No son acronimos")   
